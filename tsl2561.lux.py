7cfe3c84817dc8463a7465cc5d63840a6bbe534e
iain <iaincolledge@gmail.com>
2015-12-04 16:11:28 +0000

    Updated comments to show structure in /opt/dataloop/plugins directory

--- tsl2561.lux.py
+++ tsl2561.lux.py
@@ -2,13 +2,21 @@
 # coding=latin1
 
 '''
-This plugin requires the Dataloop agent to be in the spi and i2c groups or to be run as root.
+This plugin requires the Dataloop agent user to be in the spi and i2c groups or to be run as root.
 
 The Adafruit TSL2561 directory available as part of the Pi 2 Python library from
 
-https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code
+https://github.com/IainColledge/Adafruit-Raspberry-Pi-Python-Code
 
-Should be copied to the dataloop plugins directory /opt/dataloop/plugins and a blank file __init__.py added
+Will update to use the Adafruit library when they accept one of two TSL2561 module pull requests.
+
+The directory should be copied to the dataloop plugins directory /opt/dataloop/plugins and a blank file __init__.py added
+then this file should be copied to /opt/dataloop/plugins so the file structure looks like
+
+tsl2561.lux.py
+Adafruit_TSL2561/Adafruit_I2C.py
+Adafruit_TSL2561/Adafruit_TSL2561.py
+Adafruit_TSL2561/__init__.py
 
 Iain Colledge
 '''
