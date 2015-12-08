989eea545e587d3fd32e146bbeca4a9e5039464f
iain <iaincolledge@gmail.com>
2015-12-08 10:23:01 +0000

    Changed message construction

--- bmp085.temp_press_alt.py
+++ bmp085.temp_press_alt.py
@@ -131,7 +131,7 @@ if ((minutes == 59 and seconds < 30) or (not tmp_file())):
                 surface_pressure = altimeter
             else:
                 surface_pressure = altimeter * 33.86389
-            message = "at uptime ", days, ":", hours, ":", minutes, ":", seconds, " surface pressure ", surface_pressure, "hPa set for lat:", latitude, ", lon:", longitude, " using METAR from ", station, " at ", metar_time, " time"
+            message = "at uptime " + str(days) + ":" + str(hours) + ":" + str(minutes) + ":" + str(seconds) + " surface pressure " + str(surface_pressure) + "hPa set for lat:" + str(latitude) + ", lon:" + str(longitude) + " using METAR from " + station + " at " + metar_time + " time"
             print message
             syslog.syslog(syslog.LOG_INFO, message)
     write_cache(surface_pressure)
